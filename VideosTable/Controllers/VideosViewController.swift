9e6d0ff2b95762053daad68eeeaed9d316c5d094
Aermolaev1 <alexeyxxxerm@gmail.com>
2024-10-10 13:27:59 +0300

    Setup project

--- VideosTable/Controllers/VideosViewController.swift
+++ VideosTable/Controllers/VideosViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  VideosViewController.swift
 //  VideosTable
 //
 //  Created by Alexey on 10.10.24.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class VideosViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +16,3 @@ class ViewController: UIViewController {
 
 
 }
-
